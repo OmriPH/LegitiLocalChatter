6d9cf939c855faece0f6d38757346cf51a8bc075
PilkeySEK <PilkeySEK@gmx.de>
2024-10-25 09:22:08 +0200

    Made it add '<message>' to the chat history instead of '/lc <message>'

--- src/client/java/me/pilkeysek/legitilocalchatter/client/mixin/ChatScreenMixin.java
+++ src/client/java/me/pilkeysek/legitilocalchatter/client/mixin/ChatScreenMixin.java
@@ -13,18 +13,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
-    @Inject(at = @At("HEAD"), method = "sendMessage(Ljava/lang/String;Z)V")
+    @Inject(at = @At("HEAD"), method = "sendMessage(Ljava/lang/String;Z)V", cancellable = true)
     public void injected(String chatText, boolean addToHistory, CallbackInfo ci, @Local(argsOnly = true) LocalRef<String> refChatText) {
         if(!LegitilocalchatterClient.ENABLED) return;
 
         if(!refChatText.get().startsWith("/") && !this.normalize(refChatText.get()).isEmpty()) {
-            refChatText.set("/lc " + refChatText.get());
+            MinecraftClient.getInstance().inGameHud.getChatHud().addToMessageHistory(refChatText.get());
+            String newChatText = LegitilocalchatterClient.CHAT_PREFIX + refChatText.get();
+            refChatText.set(newChatText);
+            assert MinecraftClient.getInstance().player != null;
+            if(newChatText.startsWith("/")) {
+                MinecraftClient.getInstance().player.networkHandler.sendChatCommand(newChatText.substring(1));
+            }
+            else {
+                MinecraftClient.getInstance().player.networkHandler.sendChatMessage(newChatText);
+            }
+            ci.cancel();
         }
     }
 
